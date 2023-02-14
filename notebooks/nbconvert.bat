f05d91e86005a421830b1e038941242b5b762bbd
Cyril GABORIT <cyril.gaborit@renault.com>
2023-02-14 17:56:33 +0100

    Cut in 3 notebooks

--- notebooks/nbconvert.bat
+++ notebooks/nbconvert.bat
@@ -1,6 +1,9 @@
 @REM Notebook extraction in specified language(s)
-jupyter nbconvert --to selectLanguage --NotebookLangExporter.language=fr 1_EDA.ipynb
-jupyter nbconvert --to selectLanguage --NotebookLangExporter.language=en 1_EDA.ipynb
+jupyter nbconvert --to selectLanguage --NotebookLangExporter.language=fr 1_exploration.ipynb
+jupyter nbconvert --to selectLanguage --NotebookLangExporter.language=en 1_exploration.ipynb
 
-jupyter nbconvert --to selectLanguage --NotebookLangExporter.language=fr 2_models.ipynb
-jupyter nbconvert --to selectLanguage --NotebookLangExporter.language=en 2_models.ipynb
+jupyter nbconvert --to selectLanguage --NotebookLangExporter.language=fr 2_analysis.ipynb
+jupyter nbconvert --to selectLanguage --NotebookLangExporter.language=en 2_analysis.ipynb
+
+jupyter nbconvert --to selectLanguage --NotebookLangExporter.language=fr 3_models.ipynb
+jupyter nbconvert --to selectLanguage --NotebookLangExporter.language=en 3_models.ipynb
